e0351cf29536b9d2cf2ec02876f16bbd6814102b
b3luga15306070-hash <b3luga15306070@gmail.com>
2026-05-14 20:17:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,58 @@ def save_books(books):
         json.dump(books, file, ensure_ascii=False, indent=4)
 
 
+def add_book():
+    books = load_books()
+
+    author = input("Автор: ")
+    title = input("Название: ")
+    rating = int(input("Оценка: "))
+    date = input("Дата: ")
+
+    books.append({
+        "author": author,
+        "title": title,
+        "rating": rating,
+        "date": date
+    })
+
+    save_books(books)
+    print("Книга добавлена")
+
+
+def show_books():
+    books = load_books()
+
+    for book in books:
+        print(book["author"], "-", book["title"], "-", book["rating"])
+
+
+def average_rating():
+    books = load_books()
+
+    if not books:
+        print("Нет книг")
+        return
+
+    avg = sum(book["rating"] for book in books) / len(books)
+    print("Средняя оценка:", round(avg, 2))
+
+
+def delete_book():
+    books = load_books()
+
+    title = input("Название книги: ")
+
+    books = [
+        book for book in books
+        if book["title"].lower() != title.lower()
+    ]
+
+    save_books(books)
+
+    print("Книга удалена")
+
+
 def main():
     while True:
         print("\nТрекер книг")
@@ -39,6 +91,7 @@ def main():
 
         elif choice == "4":
             delete_book()
+
         elif choice == "5":
             print("Выход")
             break
