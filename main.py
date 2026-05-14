d801be6a0aa2daf70fddaff4e55ae67aceb9c80c
b3luga15306070-hash <b3luga15306070@gmail.com>
2026-05-14 20:01:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+def save_books(books):
+    ...
+  def add_book():
+    books = load_books()
 
+    author = input("Автор: ")
+    title = input("Название: ")
+    rating = int(input("Оценка: "))
+    date = input("Дата: ")
+
+    book = {
+        "author": author,
+        "title": title,
+        "rating": rating,
+        "date": date
+    }
+
+    books.append(book)
+
+    save_books(books)
+
+    print("Книга добавлена")
