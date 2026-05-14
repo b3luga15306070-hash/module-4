2d97221a4a5860f3e6d3663c2ef5a32d475ebcdc
b3luga15306070-hash <b3luga15306070@gmail.com>
2026-05-14 20:04:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+def show_books():
+    books = load_books()
 
+    for book in books:
+        print(
+            f"{book['author']} - "
+            f"{book['title']} - "
+            f"{book['rating']}"
+        )
+
+
+def average_rating():
+    books = load_books()
+
+    if not books:
+        print("Нет книг")
+        return
+
+    avg = sum(book["rating"] for book in books) / len(books)
+
+    print("Средняя оценка:", round(avg, 2))
