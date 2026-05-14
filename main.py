b4217aad7268951e40c96a30a959b87034a8cc8c
b3luga15306070-hash <b3luga15306070@gmail.com>
2026-05-14 19:53:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,52 @@
+import json
+import os
 
+FILE_NAME = "books.json"
+
+
+def load_books():
+    if not os.path.exists(FILE_NAME):
+        return []
+
+    with open(FILE_NAME, "r", encoding="utf-8") as file:
+        return json.load(file)
+
+
+def save_books(books):
+    with open(FILE_NAME, "w", encoding="utf-8") as file:
+        json.dump(books, file, ensure_ascii=False, indent=4)
+
+
+def main():
+    while True:
+        print("\nТрекер книг")
+        print("1. Добавить книгу")
+        print("2. Показать книги")
+        print("3. Средняя оценка")
+        print("4. Удалить книгу")
+        print("5. Выход")
+
+        choice = input("Выберите пункт: ")
+
+        if choice == "1":
+            print("Заглушка: добавить книгу")
+
+        elif choice == "2":
+            print("Заглушка: показать книги")
+
+        elif choice == "3":
+            print("Заглушка: статистика")
+
+        elif choice == "4":
+            print("Заглушка: удалить книгу")
+
+        elif choice == "5":
+            print("Выход")
+            break
+
+        else:
+            print("Неверный ввод")
+
+
+if __name__ == "__main__":
+    main()
