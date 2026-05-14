f1fd2d2e24f1423be6a521887aacc8eed24e3f1e
b3luga15306070-hash <b3luga15306070@gmail.com>
2026-05-14 20:07:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+def delete_book():
+    books = load_books()
 
+    title = input("Название книги: ")
+
+    books = [
+        book for book in books
+        if book["title"].lower() != title.lower()
+    ]
+
+    save_books(books)
+
+    print("Книга удалена")
